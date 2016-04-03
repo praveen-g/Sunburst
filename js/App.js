490ac137a8a3971b1fb2ab71df9a60c302957e5a
Praveen Gupta <praveen@Praveens-MBP.home>
2016-04-02 23:52:15 -0400

    Adding header and Patient Info

--- js/App.js
+++ js/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import ReactDom from 'react-dom' ;
 
 class App extends React.Component{
 	render() {
 		return (
 			<div>
-				<p>Sunburst</p>
-				<div>Patient Info </div>
+				<p className="header">Sunburst</p>
+				<p className="info">Patient Name: <span className="span">{this.props.name}</span></p>
+				<p className='info'>Patient MRN: <span className="span">{this.props.mrn}</span></p>
+				<p className='info'>Exam Type: <span className="span">{this.props.exam}</span></p>
 			</div>
-		)
+		);
 	}
+
 }
 
-export default App
+ReactDom.render(<App name="Praveen" mrn = "abc" exam = "CTScan"/>, document.getElementById('app'))
--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-import React from 'react';
-import ReactDom from 'react-dom'
 import App from './js/App'
 
-ReactDom.render(<App/>, document.getElementById('app'))
