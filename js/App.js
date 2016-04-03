1f84adf8f61a53b02a4051c39c0894af633b4863
Praveen Gupta <praveen@Praveens-MBP.home>
2016-04-02 21:45:59 -0400

    Adding file structure

---
+++ js/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+	render() {
+		return (
+			<div>
+				<p>Sunburst</p>
+				<div>Patient Info </div>
+			</div>
+		)
+	}
+}
+
+export default App
